cebb6dd43d001b61209403e3370e42a439f7060c
mallikagulati <20bcs3897@cuchd.in>
2023-11-04 02:18:31 +0530

    FirstCommit

---
+++ Assignment1/src/main/scala/Assignment1/Calculator.scala
@@ -0,0 +1,32 @@
+
+package Assignment1
+
+class Calculator {
+  def add(x: Int , y:Int)=
+    x+y
+
+  def subtract(x: Int, y: Int) =
+    x - y
+
+  def multiply(x: Int, y: Int) =
+    x * y
+
+  def divide(x: Int, y: Int) =
+    x / y
+
+}
+
+
+
+
+
+//object Main
+//{
+//  def main(args: Array[String])
+//  {
+//    var obj = new Add();
+//    val sum = obj.add(2,3);
+//    println(sum);
+//
+//  }
+//}
