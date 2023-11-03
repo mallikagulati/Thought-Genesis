019ffee432d4d3f12c25b238282906c02dd06d32
mallikagulati <20bcs3897@cuchd.in>
2023-11-04 02:35:09 +0530

    UpdatedTest

--- Assignment1/src/test/scala/Assignment1/CalculatorTest.scala
+++ Assignment1/src/test/scala/Assignment1/CalculatorTest.scala
@@ -4,56 +4,51 @@ import org._
 import org.scalatest.funsuite.AnyFunSuite
 
 class CalculatorTest extends AnyFunSuite {
+  val obj = new Calculator()
   test("BasicTest") {
-    val obj = new Calculator();
     assert(obj.add(2,3)===5)
   }
   test("Add0") {
-    val obj = new Calculator();
     assert(obj.add(0,0) === 0)
   }
   test("TestNegative") {
-    val obj = new Calculator();
     assert(obj.add(-1, -3) === -4)
   }
   test("BasicTestSubtraction") {
-    val obj = new Calculator();
     assert(obj.subtract(3, 2) === 1)
   }
   test("Sub0") {
-    val obj = new Calculator();
     assert(obj.subtract(8, 0) === 8)
   }
   test("TestNegativeSubtraction") {
-    val obj = new Calculator();
     assert(obj.subtract(-1, -3) === 2)
   }
 
   test("BasicTestMultiplication") {
-    val obj = new Calculator();
     assert(obj.multiply(3, 8) === 24)
   }
   test("Mul0") {
-    val obj = new Calculator();
     assert(obj.multiply(8, 0) === 0)
   }
   test("TestNegativeMultiplication") {
-    val obj = new Calculator();
     assert(obj.multiply(-4,-5) === 20)
   }
   test("BasicTestDivision") {
-    val obj = new Calculator();
     assert(obj.divide(36,12) === 3)
   }
   test("Div0") {
-    val obj = new Calculator();
     assert(obj.divide(0,9) === 0)
   }
   test("TestNegativeDivision") {
-    val obj = new Calculator();
     assert(obj.divide(-45, -5) === 9)
   }
 
+  test("Divisionby0")
+  {
+    assertThrows[ArithmeticException]{
+      obj.divide(6,0)
+    }
+  }
 }
 
 
